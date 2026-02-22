14929ffa3f600258e850440124facf1aa7f5408a
justinecua <jcua.dev@gmail.com>
2026-02-22 19:43:04 +0800

    header update for redirection

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,14 +1,15 @@
 import Tabs from "./Tabs";
 import ThemeToggle from "../ThemeToggle";
 import logo from "../../assets/logo_portfolio.jpeg";
+import { Link } from "react-router-dom";
 
 function Header({ tabs, minimized }) {
   return (
     <header className={`header ${minimized ? "header-min" : ""}`}>
-      <div className="header-left">
+      <Link to="/" className="header-left" aria-label="Go to About">
         <img src={logo} alt="Justine Cua Logo" className="logo-img" />
         <span className="tagline">Justine Cua</span>
-      </div>
+      </Link>
 
       <div className="header-center">
         <Tabs tabs={tabs} />
